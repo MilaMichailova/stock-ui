516df8cee3f4d914868ada2444df6c197fa2ebce
MilaMichailova <kosarevitch.mila2010@gmail.com>
2023-08-28 12:40:47 +0300

    error in create reacive Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { Routes, RouterModule } from '@angular/router';
-import { LoginFormComponent } from './authorization/components/login-form/login-form.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-const appRoutes: Routes = [
-  { path: '', component: AppComponent },
-  { path: 'login', component: LoginFormComponent },
-];
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes)],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/authorization/components/login-form/login-form.component.ts
+++ src/app/authorization/components/login-form/login-form.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormArray,
+  FormBuilder,
+} from '@angular/forms';
 
 @Component({
-  selector: 'form',
+  selector: 'login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.scss']
+  styleUrls: ['./login-form.component.scss'],
 })
 export class LoginFormComponent {
+  public login(): void {
+    console.log('меня отправили!');
+  }
 
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = formBuilder.group({
+      login: [' ', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  // isControlInvalid(controlName: string): boolean {
+  //   const control = this.loginForm.controls[controlName];
+
+  //   const result = control.invalid && control.touched;
+
+  //   return result;
+  // }
+
+  submit() {
+    console.log(this.loginForm);
+  }
 }
