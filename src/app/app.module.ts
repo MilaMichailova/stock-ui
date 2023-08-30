713cd4e0cab1d5a3f11386ab3ac48ff702de6047
MilaMichailova <kosarevitch.mila2010@gmail.com>
2023-08-30 08:57:13 +0300

    сделаны валидаторы на обязательность заполнения полей и сообщение об ошибке и подсветку полей ввода

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AuthorizationModule } from './authorization/authorization.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AuthorizationModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/authorization/components/login-form/login-form.component.ts
+++ src/app/authorization/components/login-form/login-form.component.ts
@@ -7,34 +7,54 @@ import {
   FormBuilder,
 } from '@angular/forms';
 
+interface ILoginForm {
+  login: FormControl<string | null>;
+  password: FormControl<string | null>;
+}
+
 @Component({
   selector: 'login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss'],
 })
 export class LoginFormComponent {
+  loginForm: FormGroup<ILoginForm>;
+
+  private hasLoginErrors: boolean;
+
+  public get isFormInvalid(): boolean {
+    return this.hasLoginErrors;
+  }
+
+  public get canLogin(): boolean {
+    return (
+      this.loginForm.controls.login.valid &&
+      this.loginForm.controls.password.valid
+    );
+  }
+
+  constructor() {
+    this.initLoginForm();
+  }
+
   public login(): void {
-    console.log('меня отправили!');
+    const loginInfo = this.loginForm.value;
+
+    if (loginInfo.login !== 'admin') {
+      this.loginForm.controls.password.reset();
+      this.loginForm.controls.password.markAsTouched();
+      this.hasLoginErrors = true;
+    }
   }
 
-  loginForm: FormGroup;
+  public clearErrors(): void {
+    this.hasLoginErrors = false;
+  }
 
-  constructor(private formBuilder: FormBuilder) {
-    this.loginForm = formBuilder.group({
-      login: [' ', [Validators.required]],
-      password: ['', [Validators.required]],
+  private initLoginForm(): void {
+    this.loginForm = new FormGroup<ILoginForm>({
+      login: new FormControl<string>('', [Validators.required]),
+      password: new FormControl<string>('', [Validators.required]),
     });
-  }
-
-  // isControlInvalid(controlName: string): boolean {
-  //   const control = this.loginForm.controls[controlName];
-
-  //   const result = control.invalid && control.touched;
-
-  //   return result;
-  // }
-
-  submit() {
-    console.log(this.loginForm);
   }
 }
